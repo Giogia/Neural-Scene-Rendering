94562e63f5cc1324bf87587b17990b4fb1468c1f
Giovanni Tommasi <giovannitommasi95@gmail.com>
2020-04-23 19:38:13 +0200

    fix: set current as model cache directory

--- train.py
+++ train.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     aeloss = profile.get_loss()
 
     # max lr test
-    lr_finder = LRFinder(ae, aeoptim, aeloss, lossweights, device=device)
+    lr_finder = LRFinder(ae, aeoptim, aeloss, lossweights, device=device, cache_dir=outpath)
     lr_finder.range_test(dataloader, end_lr=0.05, num_iter=30)
     lr_finder.plot()
     lr_finder.reset()
