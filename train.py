3d3b2aeac46759f14df33c604f8dfe647108a32b
Giovanni Tommasi <giovannitommasi95@gmail.com>
2020-05-13 17:46:05 +0200

    Update train.py

--- train.py
+++ train.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
         scheduler = checkpoint['scheduler']
     else:
         base_lr = ae_optimizer.param_groups[0]['lr']
-        max_lr = 2.5e-4 if args.super else base_lr
+        max_lr = 2e-4 if args.super else base_lr
         scheduler = torch.optim.lr_scheduler.CyclicLR(ae_optimizer, base_lr, max_lr,
                                                       step_size_up=100, step_size_down=100, cycle_momentum=False)
         # scheduler = torch.optim.lr_scheduler.OneCycleLR(ae_optimizer, max_lr=max_lr, total_steps=profile.max_iter)
