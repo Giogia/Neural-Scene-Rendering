eba959fd3b98151b835c72727cdf61be310fa5a4
Giovanni Tommasi <giovannitommasi95@gmail.com>
2020-05-22 17:52:32 +0200

    Update config.py

--- experiments/Fox/experiment1/config.py
+++ experiments/Fox/experiment1/config.py
@@ -109,7 +109,7 @@ class Render:
 
     e.g., python render.py {configpath} Render --maxframes 128"""
 
-    def __init__(self, cam=None, show_target=False, view_template=True):
+    def __init__(self, cam=None, show_target=True, view_template=False):
         self.cam = cam
         self.show_target = show_target
         self.view_template = view_template
