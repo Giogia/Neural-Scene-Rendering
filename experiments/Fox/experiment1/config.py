f93263ecaf3f9b4888505490cb0f7e01aa59d111
Giovanni Tommasi <giovannitommasi95@gmail.com>
2020-06-01 18:55:28 +0200

    Update config.py

--- experiments/Fox/experiment1/config.py
+++ experiments/Fox/experiment1/config.py
@@ -41,7 +41,7 @@ def get_autoencoder(dataset):
 # A profile is instantiated by the training or evaluation scripts
 # and controls how the dataset and autoencoder is created
 class Train:
-    batch_size = 8
+    batch_size = 4
     max_iter = 10000
 
     def get_autoencoder(self, dataset): return get_autoencoder(dataset)
