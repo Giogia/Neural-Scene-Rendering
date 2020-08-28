1a9476e46c56c3b0fa13277b4d04fd52461d59b5
Giovanni Tommasi <giovannitommasi95@gmail.com>
2020-08-28 13:25:56 +0200

    Update autoencoder.py

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -192,9 +192,9 @@ class Autoencoder(nn.Module):
                 if pixel_coords.size()[1:3] != depth.size()[2:4]:
                     depth = F.grid_sample(depth, sample_coords, align_corners=False)
 
-                err = weight ** 2 * (log(depth) - log(ray_length))
-                i_depth_sqerr = weight * (log(depth) - log(ray_length)) ** 2 \
-                                - torch.sum(err.view(err.size(0), -1), dim=-1) ** 2
+
+                i_depth_sqerr = weight * (log(depth) - log(ray_length)) ** 2
+                i_depth_sqerr_2 = weight * (log(depth) - log(ray_length))
 
                 # i_depth_sqerr = weight * (depth - ray_length) ** 2
 
@@ -202,7 +202,8 @@ class Autoencoder(nn.Module):
                     result["i_depth_sqerr"] = i_depth_sqerr
 
                 if "i_depth_mse" in loss_list:
-                    i_depth_mse = torch.sum(i_depth_sqerr.view(i_depth_sqerr.size(0), -1), dim=-1)
+                    i_depth_mse = torch.sum(i_depth_sqerr.view(i_depth_sqerr.size(0), -1), dim=-1) \
+                                  - torch.sum(i_depth_sqerr_2.view(i_depth_sqerr_2.size(0), -1), dim=-1) ** 2
                     i_depth_mse_weight = torch.sum(weight.view(weight.size(0), -1), dim=-1)
 
                     result["losses"]["i_depth_mse"] = (i_depth_mse, i_depth_mse_weight)
